3b6bc5e6b5d6ee38f3fbf6342c4ee7b6d675a266
Gabriel Sousa <gabrieldesousa05@gmail.com>
2021-12-04 17:54:57 -0300

    relacionamento entre tabelas

---
+++ src/main/java/org/generation/blogpessoal/BlogPessoal39Application.java
@@ -0,0 +1,13 @@
+package org.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoal39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoal39Application.class, args);
+	}
+
+}
